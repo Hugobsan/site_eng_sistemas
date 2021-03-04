2150feaf6253ea68802064f3eecdf479d62245e1
Daniel Araújo <dfdaa0@outlook.com>
2021-03-04 14:13:31 -0300

    Added content to rodape.php
    Creation and styling of the content loaded in the footer of the page.

---
+++ rodape.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/rodape.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Title</title>
+</head>
+<body>
+    <div class="content">   
+        <footer id="rodape">
+            <span class="texto-do-rodape col-lg-6 col-sm-12">Desenvolvido por:
+                <div class="div-first">
+                    Hugo Barbosa &#x00028;<a href="#" target="_blank">LinkedIn</a>&#x00009;/&#x00009;<a href="https://github.com/Hugobsan" target="_blank">GitHub</a>&#x00029;
+                </div>
+                <div class="div-second"> 
+                    Daniel Araújo &#x00028;<a href="https://www.linkedin.com/in/daniel-ara%C3%BAjo-570b1b1b1/" target="_blank">LinkedIn</a>&#x00009;/&#x00009;<a href="https://github.com/dfdaa0" target="_blank">GitHub</a>&#x00029;
+                </div>
+            </span>
+        </footer>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
